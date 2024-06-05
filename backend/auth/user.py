fed07f1c576d2d86a13117a9a967bf0eb2cf83ef
Brett A. Thomas <quark@baz.com>
2024-06-04 23:11:27 -0700

    Changes to support user impersonation by internal users.
    UserRequest now returns an optional internal_email
    
    create_new_user_session() now can optionally take an internal_email of
    the person impersonating another.
    
    authenticate_token() now returns a Tuple of the logged in user, and
    the optional internal user who is impersonating them
    
    get_current_user() is similarly changed to return both the external
    and internal user (if any).
    
    authenticate_user() also returns both users if they are there.

--- backend/auth/user.py
+++ backend/auth/user.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 import uuid
-from typing import Optional
+from typing import Optional, Tuple
 
 import jwt
 from fastapi import HTTPException
@@ -20,18 +20,19 @@ from log import logger
 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
-
 class UserRequest(BaseModel):
     email: str
     name: Optional[str]
     organization: Optional[str]
-
+    internal_email: Optional[str]
 
 def create_or_fetch_user(user_request: UserRequest) -> str:
     with read_write_session() as session:
         user = session.query(User).filter_by(email=user_request.email).first()
         if user:
-            token = create_new_user_session(session, user.email)
+            token = create_new_user_session(session,
+                                            user.email,
+                                            user_request.internal_email)
             return token
         elif user_request.organization and user_request.name and user_request.email:
             organization = Organization(
@@ -59,14 +60,37 @@ def create_or_fetch_user(user_request: UserRequest) -> str:
             return create_new_user_session(session, user_request.email)
 
 
-def create_new_user_session(session: Session, email: str) -> str:
+def create_new_user_session(session: Session,
+                            email: str,
+                            internal_email: str = None) -> str:
     user = session.query(User).filter_by(email=email).first()
     if user is None:
         raise ValueError(f"No user found with email - {email}")
-    jti, jwt_token = create_access_token(user.id)
-    store_user_session(user.id, jti)
-    return jwt_token
 
+    internal_user =\
+        session.query(User).filter(User.email == internal_email).first()
+
+    internal_user_id = None
+
+    if internal_user is not None:
+        #This is a request by an "internal user" to impersonate another user.
+        #We need to check some things. ARE they an internal user? And is
+        #the targeted user NOT an internal user?
+        if internal_user.is_internal is not True:
+            raise PermissionError(f"User {internal_email} is not " +\
+                                  "an internal user. Permission denied.")
+
+        if user.is_internal is True:
+            raise PermissionError(f"User {email} is not " +\
+                                  "an external user. Permission denied.")
+
+        internal_user_id = internal_user.id
+
+    jti, jwt_token = create_access_token(user.id)
+
+    store_user_session(user.id, jti, internal_user_id)
+
+    return jwt_token
 
 def logout_user_session(user_id: str, jti: str):
     with read_write_session() as session:
@@ -88,16 +112,16 @@ def logout_user_session(user_id: str, jti: str):
         logger.info(f"User logged out: {user_id}")
 
 
-def authenticate_token(token: str) -> Optional[User]:
+def authenticate_token(token: str) -> Tuple[Optional[User], Optional[User]]:
     try:
         payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=JWT_ALGORITHM)
         jti = payload["jti"]
     except jwt.ExpiredSignatureError as e:
         logger.warning(f"Expired JWT token: {e}")
-        return None
+        return None, None
     except jwt.InvalidTokenError as e:
         logger.error(f"Invalid JWT token: {e}")
-        return None
+        return None, None
 
     with read_only_session() as session:
         result = (
@@ -107,17 +131,20 @@ def authenticate_token(token: str) -> Optional[User]:
             .first()
         )
         if result and result.end_at is None:
-            return result.user
+            internal_user = result.internal_user \
+                if result.internal_user_id is not None else None
+
+            return result.user, internal_user
         else:
             logger.warning(f"Failed authentication attempt with token: {jti}")
-            return None
+            return None, None
 
 
-def get_current_user(request: Request) -> Optional[User]:
+def get_current_user(request: Request) -> Tuple[Optional[User], Optional[User]]:
     jwt_token = request.cookies.get("access_token")
     if jwt_token:
         return authenticate_token(jwt_token)
-    return None
+    return None, None
 
 
 def decode_jwt(token: str, field: str) -> Optional[str]:
@@ -138,15 +165,15 @@ def get_jti_from_request(request: Request) -> Optional[str]:
     return None
 
 
-def authenticate_user(request: Request) -> User:
+def authenticate_user(request: Request) -> Tuple[[User], [User]]:
     """
     Returns the user if the token is valid and the user is active, else raises an exception
     """
-    user = get_current_user(request)
+    (user, internal_user) = get_current_user(request)
 
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="UNAUTHORIZED")
 
     if user.status != UserStatus.ACTIVE:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="ORGANIZATION_REQUIRED")
-    return user
+    return user, internal_user
